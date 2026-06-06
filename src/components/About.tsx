cc57012feb862912ba7d6bb391180e77127d7b92
MrFei-the-villain <fei.ying.314@gmail.com>
2026-06-06 18:39:27 +0800

    Bump version to 1.1.0

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -25,7 +25,7 @@ export function About({ addToast: _addToast }: AboutProps) {
           </svg>
           <div>
             <h2>Window Layout Manager</h2>
-            <p className="about-version">Version 1.0.3</p>
+            <p className="about-version">Version 1.1.0</p>
           </div>
         </div>
 
